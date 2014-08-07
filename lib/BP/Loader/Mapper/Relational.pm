2fdb736a8e34f2b2df21c33bde0b1274385c0471
José María Fernández <jmfernandez@cnio.es>
2014-08-07 19:20:35 +0200

    Overhaul on _genDestination and _bulkInsert as now we have one concept to many tables reality

--- lib/BP/Loader/Mapper/Relational.pm
+++ lib/BP/Loader/Mapper/Relational.pm
@@ -8,6 +8,7 @@ use DBI;
 use File::Basename;
 use File::Spec;
 use JSON;
+use Tie::IxHash;
 use XML::LibXML;
 
 use BP::Model;
@@ -30,10 +31,11 @@ use constant {
 	CONF_PORT	=>	'port',
 	CONF_DBUSER	=>	'user',
 	CONF_DBPASS	=>	'pass',
+	CONF_DIALECT	=>	'sql-dialect',
 };
 
 my @DEFAULTS = (
-	['sql-dialect' => 'mysql'],
+	[BP::Loader::Mapper::Relational::CONF_DIALECT => 'mysql'],
 	['batch-size' => 4096],
 	[BP::Loader::Mapper::Relational::CONF_DB	=> undef ],
 	[BP::Loader::Mapper::Relational::CONF_HOST	=> '' ],
@@ -68,7 +70,7 @@ my %ABSTYPE2SQL = (
 );
 
 my %ABSTYPE2SQLKEY = %ABSTYPE2SQL;
-$ABSTYPE2SQLKEY{BP::Model::ColumnType::STRING_TYPE} = ['VARCHAR(128)',DBI::SQL_VARCHAR];
+$ABSTYPE2SQLKEY{BP::Model::ColumnType::STRING_TYPE} = ['VARCHAR(128)',DBI::SQL_VARCHAR,\&__bypass];
 
 
 my @FALSE_TRUE = ('FALSE','TRUE');
@@ -125,6 +127,10 @@ my %SQLDIALECTS = (
 				],
 );
 
+use constant {
+	KEY_COLUMN_TEMPLATE	=>	'_keyColumnTemplate',
+	INDEX_COLUMN_TEMPLATE	=>	'_indexColumnTemplate',
+};
 
 # Constructor parameters:
 #	model: a BP::Model instance
@@ -157,8 +163,41 @@ sub new($$) {
 			Carp::croak("ERROR: Unable to read section $SECTION");
 		}
 	}
-	Carp::croak("ERROR: Unknown SQL dialect '$self->{'sql-dialect'}'. Valid ones are: ".join(', ',keys(%SQLDIALECTS)))  unless(exists($SQLDIALECTS{$self->{'sql-dialect'}}));
-	$self->{dialect} = $SQLDIALECTS{$self->{'sql-dialect'}};
+	Carp::croak("ERROR: Unknown SQL dialect '$self->{BP::Loader::Mapper::Relational::CONF_DIALECT}'. Valid ones are: ".join(', ',keys(%SQLDIALECTS)))  unless(exists($SQLDIALECTS{$self->{BP::Loader::Mapper::Relational::CONF_DIALECT}}));
+	$self->{__dialect} = $SQLDIALECTS{$self->{BP::Loader::Mapper::Relational::CONF_DIALECT}};
+	
+	#########
+	# These definitions are needed to generate the proper SQL sentences for hashes and arrays
+	#########
+	my $kdoc = XML::LibXML::Document->new('1.0','UTF-8');
+	my $root = $kdoc->createElementNS(BP::Model::dccNamespace,'column-set');
+	$kdoc->setDocumentElement($root);
+	
+	my $keyColumnElem = $kdoc->createElementNS(BP::Model::dccNamespace,'column');
+	$keyColumnElem->setAttribute('name','key');
+	$root->appendChild($keyColumnElem);
+
+	my $keyCT = $kdoc->createElementNS(BP::Model::dccNamespace,'column-type');
+	$keyCT->setAttribute('column-kind','idref');
+	$keyCT->setAttribute('item-type','string');
+	$keyColumnElem->appendChild($keyCT);
+	
+	my $indexColumnElem = $kdoc->createElementNS(BP::Model::dccNamespace,'column');
+	$indexColumnElem->setAttribute('name','idx');
+	$root->appendChild($indexColumnElem);
+	
+	my $indexCT = $kdoc->createElementNS(BP::Model::dccNamespace,'column-type');
+	$indexCT->setAttribute('column-kind','idref');
+	$indexCT->setAttribute('item-type','integer');
+	$indexColumnElem->appendChild($indexCT);
+	
+	# This one is for hashes
+	my $keyColumn = BP::Model::Column->new('key',undef,undef,BP::Model::ColumnType->parseColumnType($keyColumnElem,$model,'dummy-key'));
+	# This one is for arrays (at this moment, only unidimensional ones)
+	my $indexColumn = BP::Model::Column->new('idx',undef,undef,BP::Model::ColumnType->parseColumnType($indexColumnElem,$model,'dummy-i'));
+	
+	$self->{BP::Loader::Mapper::Relational::KEY_COLUMN_TEMPLATE} = $keyColumn;
+	$self->{BP::Loader::Mapper::Relational::INDEX_COLUMN_TEMPLATE} = $indexColumn;
 	
 	return $self;
 }
@@ -242,47 +281,20 @@ sub generateNativeModel($) {
 	
 	my $workingDir = shift;
 	my $filePrefix = $self->{BP::Loader::Mapper::FILE_PREFIX_KEY};
+	
+	my $keyColumn = $self->{BP::Loader::Mapper::Relational::KEY_COLUMN_TEMPLATE};
+	my $indexColumn = $self->{BP::Loader::Mapper::Relational::INDEX_COLUMN_TEMPLATE};
+	
 	my $fullFilePrefix = File::Spec->catfile($workingDir,$filePrefix);
 	
 #	model: a BP::Model instance, with the parsed model.
 	my $model = $self->{model};
 #	the path to the SQL output file
-	my $outfileSQL = $fullFilePrefix.'-'.$self->{'sql-dialect'}.'.sql';
+	my $outfileSQL = $fullFilePrefix.'-'.$self->{BP::Loader::Mapper::Relational::CONF_DIALECT}.'.sql';
 #	the path to the SQL script which joins 
-	my $outfileTranslateSQL = $fullFilePrefix.'_CVtrans'.'-'.$self->{'sql-dialect'}.'.sql';
+	my $outfileTranslateSQL = $fullFilePrefix.'_CVtrans'.'-'.$self->{BP::Loader::Mapper::Relational::CONF_DIALECT}.'.sql';
 	
-	#########
-	# These definitions are needed to generate the proper SQL sentences for hashes and arrays
-	#########
-	my $kdoc = XML::LibXML::Document->new('1.0','UTF-8');
-	my $root = $kdoc->createElementNS(BP::Model::dccNamespace,'column-set');
-	$kdoc->setDocumentElement($root);
-	
-	my $keyColumnElem = $kdoc->createElementNS(BP::Model::dccNamespace,'column');
-	$keyColumnElem->setAttribute('name','key');
-	$root->appendChild($keyColumnElem);
-
-	my $keyCT = $kdoc->createElementNS(BP::Model::dccNamespace,'column-type');
-	$keyCT->setAttribute('column-kind','idref');
-	$keyCT->setAttribute('item-type','string');
-	$keyColumnElem->appendChild($keyCT);
-	
-	my $indexColumnElem = $kdoc->createElementNS(BP::Model::dccNamespace,'column');
-	$indexColumnElem->setAttribute('name','idx');
-	$root->appendChild($indexColumnElem);
-	
-	my $indexCT = $kdoc->createElementNS(BP::Model::dccNamespace,'column-type');
-	$indexCT->setAttribute('column-kind','idref');
-	$indexCT->setAttribute('item-type','integer');
-	$indexColumnElem->appendChild($indexCT);
-	
-	# This one is for hashes
-	my $keyColumn = BP::Model::Column->new('key',undef,undef,BP::Model::ColumnType->parseColumnType($keyColumnElem,$model,'dummy-key'));
-	# This one is for arrays (at this moment, only unidimensional ones)
-	my $indexColumn = BP::Model::Column->new('idx',undef,undef,BP::Model::ColumnType->parseColumnType($indexColumnElem,$model,'dummy-i'));
-	
-	
-	my $dialectFuncs = $self->{dialect};
+	my $dialectFuncs = $self->{__dialect};
 	my $p_TYPE2SQL = $dialectFuncs->[_SQLDIALECT_TYPE2SQL];
 	my $p_TYPE2SQLKEY = $dialectFuncs->[_SQLDIALECT_TYPE2SQLKEY];
 	my($FALSE_VAL,$TRUE_VAL) = @{$dialectFuncs->[_SQLDIALECT_FALSE_TRUE]};
@@ -295,7 +307,7 @@ sub generateNativeModel($) {
 	my $aliasType = $p_TYPE2SQL->{BP::Model::ColumnType::BOOLEAN_TYPE}[0];
 	
 	if(open(my $SQL,'>:utf8',$outfileSQL)) {
-		print $SQL '-- File '.File::Basename::basename($outfileSQL)." (".$self->{'sql-dialect'}." dialect)\n";
+		print $SQL '-- File '.File::Basename::basename($outfileSQL)." (".$self->{BP::Loader::Mapper::Relational::CONF_DIALECT}." dialect)\n";
 		print $SQL '-- Generated from '.$model->projectName.' '.$model->versionString."\n";
 		print $SQL '-- '.localtime()."\n";
 		
@@ -407,8 +419,10 @@ sub generateNativeModel($) {
 				
 				my $newtable = $basename.'_'.$column->name;
 				# Inject always a column "index" or "key"
-				my $ikey = $column->columnType->containerType==BP::Model::ColumnType::ARRAY_CONTAINER?$indexColumn:$keyColumn;
-				$refColumnSet->addColumn($ikey->clone(undef,$newtable.'_'),1);
+				if($column->columnType->containerType==BP::Model::ColumnType::ARRAY_CONTAINER || $column->columnType->containerType==BP::Model::ColumnType::HASH_CONTAINER) {
+					my $ikey = $column->columnType->containerType==BP::Model::ColumnType::ARRAY_CONTAINER?$indexColumn:$keyColumn;
+					$refColumnSet->addColumn($ikey->clone(undef,$newtable.'_'),1);
+				}
 				
 				if(blessed($column->columnType->restriction) && $column->columnType->restriction->isa('BP::Model::CompoundType')) {
 					$refColumnSet->addColumns($column->columnType->restriction->columnSet,1);
@@ -443,7 +457,7 @@ sub generateNativeModel($) {
 		
 		# Now, the CVs and the columns using them
 		if(open(my $TSQL,'>:utf8',$outfileTranslateSQL)) {
-			print $TSQL '-- File '.File::Basename::basename($outfileTranslateSQL)." (".$self->{'sql-dialect'}." dialect)\n";
+			print $TSQL '-- File '.File::Basename::basename($outfileTranslateSQL)." (".$self->{BP::Loader::Mapper::Relational::CONF_DIALECT}." dialect)\n";
 			print $TSQL '-- Generated from '.$model->projectName.' '.$model->versionString."\n";
 			print $TSQL '-- '.localtime()."\n";
 			
@@ -715,12 +729,12 @@ sub _dsn() {
 	my @dsnValues=();
 	
 	foreach my $key (CONF_DB,CONF_HOST,CONF_PORT) {
-		if(exists($self->{$key}) && defined($self->{$key}) && $self->{$key} ne '' && exists($self->{dialect}[_SQLDIALECT_DSNPARAMS]{$key})) {
-			push(@dsnValues,$self->{dialect}[_SQLDIALECT_DSNPARAMS]{$key} .'='.$self->{$key});
+		if(exists($self->{$key}) && defined($self->{$key}) && $self->{$key} ne '' && exists($self->{__dialect}[_SQLDIALECT_DSNPARAMS]{$key})) {
+			push(@dsnValues,$self->{__dialect}[_SQLDIALECT_DSNPARAMS]{$key} .'='.$self->{$key});
 		}
 	}
 	
-	return join(':','dbi',$self->{dialect}[_SQLDIALECT_DRIVER],join(';',@dsnValues));
+	return join(':','dbi',$self->{__dialect}[_SQLDIALECT_DRIVER],join(';',@dsnValues));
 }
 
 # This method returns a connection to the database
@@ -793,22 +807,86 @@ sub _genDestination($) {
 	
 	my $correlatedConcept = shift;
 	
+	my $dbh = $self->connect();
+	my $keyColumn = $self->{BP::Loader::Mapper::Relational::KEY_COLUMN_TEMPLATE};
+	my $indexColumn = $self->{BP::Loader::Mapper::Relational::INDEX_COLUMN_TEMPLATE};
+	my $dialectFuncs = $self->{__dialect};
+	my $p_TYPE2SQL = $dialectFuncs->[_SQLDIALECT_TYPE2SQL];
+	my $p_TYPE2SQLKEY = $dialectFuncs->[_SQLDIALECT_TYPE2SQLKEY];
+	
+	# The order must be preserved, and so we are using a Tie::IxHash for $destination
+	my %destHash = ();
+	tie(%destHash,'Tie::IxHash');
+	
+	my $__genDest = undef;
+	$__genDest = sub($$;\@$) {
+		my($basename,$columnSet,$p_colorder,$concept) = @_;
+		
+		$p_colorder = $columnSet->columnNames  unless(defined($p_colorder));
+		
+		my @subcolumns = ();
+		
+		my @columnsToInsert = @{$columnSet->columns}{@{$p_colorder}};
+		my $idx = 0;
+		foreach my $column (@columnsToInsert) {
+			$idx++;
+			unless($column->columnType->containerType==BP::Model::ColumnType::SCALAR_CONTAINER && !(blessed($column->columnType->restriction) && $column->columnType->restriction->isa('BP::Model::CompoundType'))) {
+				if($column->columnType->containerType==BP::Model::ColumnType::SCALAR_CONTAINER) {
+					# It only happens to compound types
+					my $rColumnSet = $column->columnType->restriction->columnSet;
+					
+					splice(@columnsToInsert,$idx,0,map { $_->clone(undef,$column->name.'_') } @{$rColumnSet->columns}{@{$rColumnSet->columnNames}});
+				} else {
+					
+					# These are defined in a separate table
+					push(@subcolumns,$column);
+				}
+			}
+		}
+		
+		my @colnames = map { $_->name } @columnsToInsert;
+		my @coltypes = map { (($_->columnType->use == BP::Model::ColumnType::IDREF || defined($_->refColumn))?$p_TYPE2SQLKEY:$p_TYPE2SQL)->{$_->columnType->type}[1] } @columnsToInsert;
+		my @colmanglers = map { (($_->columnType->use == BP::Model::ColumnType::IDREF || defined($_->refColumn))?$p_TYPE2SQLKEY:$p_TYPE2SQL)->{$_->columnType->type}[2] } @columnsToInsert;
+				
+		my $insertSentence = 'INSERT INTO '.$basename.'('.join(',',@colnames).') VALUES ('.join(',', map { '?' } @columnsToInsert).')';
+		
+		$destHash{$basename} = [$dbh->prepare($insertSentence),\@colnames,\@coltypes,\@colmanglers];
+		
+		# And now, let's process complicated columns
+		foreach my $column (@subcolumns) {
+			my $refColumnSet = $columnSet->idColumns($concept);
+			my $numIdColumns = scalar(@{$refColumnSet->idColumnNames});
+			
+			my $newtable = $basename.'_'.$column->name;
+			# Inject always a column "index" or "key"
+			if($column->columnType->containerType==BP::Model::ColumnType::ARRAY_CONTAINER || $column->columnType->containerType==BP::Model::ColumnType::HASH_CONTAINER) {
+				my $ikey = $column->columnType->containerType==BP::Model::ColumnType::ARRAY_CONTAINER?$indexColumn:$keyColumn;
+				$refColumnSet->addColumn($ikey->clone(undef,$newtable.'_'),1);
+			}
+			
+			if(blessed($column->columnType->restriction) && $column->columnType->restriction->isa('BP::Model::CompoundType')) {
+				$refColumnSet->addColumns($column->columnType->restriction->columnSet,1);
+			} else {
+				my $newCol = $column->clone(undef,undef,1);
+				$refColumnSet->addColumn($newCol,1);
+			}
+			
+			$__genDest->($newtable,$refColumnSet);
+		}
+	};
+	
 	my $concept = $correlatedConcept->concept;
 	my $desttable = __entryName($concept);
 	
 	my @colorder = BP::Loader::Mapper::_fancyColumnOrdering($concept);
 	my $columnSet = $concept->columnSet;
 	
-	my $insertSentence = 'INSERT INTO '.$desttable.'('.join(',',map { $_->name } @{$columnSet->columns}{@colorder}).') VALUES ('.join(',', map { '?' } @colorder).')';
-	
-	my $dbh = $self->connect();
-	
-	my $destination = $dbh->prepare($insertSentence);
+	$__genDest->($desttable,$columnSet,\@colorder,$concept);
 	
 	# Starting a transaction so it is all or nothing
 	$dbh->begin_work();
 	
-	return $destination;
+	return \%destHash;
 }
 
 # _freeDestination parameters:
@@ -832,8 +910,11 @@ sub _freeDestination(;$) {
 		$dbh->commit();
 	}
 	
-	# Freeing the sentence
-	$destination->finish();
+	# Freeing the sentence(s)
+	foreach my $destInfo (values(%{$destination})) {
+		$destInfo->[0]->finish();
+		$destInfo->[0] = undef;
+	}
 }
 
 # _bulkPrepare parameters:
@@ -849,7 +930,7 @@ sub _bulkPrepare($$) {
 	my $entorp = shift;
 	
 	my $concept = $correlatedConcept->concept();
-	my $dialectFuncs = $self->{dialect};
+	my $dialectFuncs = $self->{__dialect};
 	my $p_TYPE2SQL = $dialectFuncs->[_SQLDIALECT_TYPE2SQL];
 	my $p_TYPE2SQLKEY = $dialectFuncs->[_SQLDIALECT_TYPE2SQLKEY];
 	
@@ -860,14 +941,11 @@ sub _bulkPrepare($$) {
 	foreach my $colname (@colorder) {
 		my $column = $columnSet->columns->{$colname};
 		my $columnType = $column->columnType;
-		my $DBItype = undef;
 		my $mangler = undef;
 		if($columnType->use == BP::Model::ColumnType::IDREF || defined($column->refColumn)) {
-			$DBItype = $p_TYPE2SQLKEY->{$columnType->type}[1];
 			$mangler = $p_TYPE2SQLKEY->{$columnType->type}[2];
 		} else {
-			$DBItype = $p_TYPE2SQL->{$columnType->type}[1];
-			$mangler = $p_TYPE2SQLKEY->{$columnType->type}[2];
+			$mangler = $p_TYPE2SQL->{$columnType->type}[2];
 		}
 		
 		# FUTURE: improve these two cases, based on non-standard database features
@@ -888,7 +966,7 @@ sub _bulkPrepare($$) {
 			push(@preparedData,$mangler->($_[0]));
 		}
 		;
-		push(@coldata,[\@preparedData,$DBItype]);
+		push(@coldata,\@preparedData);
 		push(@pushorder,$pusher);
 	}
 	
@@ -906,7 +984,7 @@ sub _bulkPrepare($$) {
 
 # _bulkInsert parameters:
 #	destination: The destination of the bulk insertion, which is a DBI prepared statement.
-#	bulkData: a reference to an array of hashes which contain the values to store.
+#	bulkData: a reference to a hash of arrays which contain the values to store.
 sub _bulkInsert($\@) {
 	my $self = shift;
 	
@@ -915,15 +993,35 @@ sub _bulkInsert($\@) {
 	my $destination = shift;
 	my $bulkData = shift;
 	
-	Carp::croak("ERROR: bulkInsert needs a prepared statement")  unless(blessed($destination) && $destination->can('execute'));
+	Carp::croak("ERROR: _bulkInsert needs the destination as a hash of prepared statements")  unless(ref($destination) eq 'HASH');
+	Carp::croak("ERROR: _bulkInsert needs the data as a hash")  unless(ref($bulkData) eq 'HASH');
 	
-	my $colnum = 1;
-	foreach my $p_column (@{$bulkData}) {
-		$destination->bind_param_array($colnum,$p_column->[0],$p_column->[1]);
-		$colnum++;
+	# The order is preserved because we are using a Tie::IxHash for $destination
+	foreach my $bulkKey (keys(%{$destination})) {
+		if(exists($bulkData->{$bulkKey})) {
+			my $bulkArray = $bulkData->{$bulkKey};
+			my $destInfo = $destination->{$bulkKey};
+			my $destSentence = (ref($destInfo) eq 'ARRAY' && exists($destInfo->[0]))?$destInfo->[0]:undef;
+			
+			Carp::croak("ERROR: _bulkInsert needs a prepared statement")  unless(blessed($destSentence) && $destSentence->can('execute'));
+			Carp::croak("ERROR: _bulkInsert needs an array of arrays for the prepared statement")  unless(ref($bulkArray) eq 'ARRAY');
+			
+			my $colnum = 1;
+			foreach my $p_column (@{$bulkArray}) {
+				my $mangler = $destInfo->[3][$colnum-1];
+				
+				# Preparing the data for SQL
+				foreach my $coldata (@{$p_column}) {
+					$coldata = $mangler->($coldata);
+				}
+				
+				$destSentence->bind_param_array($colnum,$p_column,$destInfo->[2][$colnum-1]);
+				$colnum++;
+			}
+			
+			$destSentence->execute_array();
+		}
 	}
-	
-	$destination->execute_array();
 }
 
 1;
